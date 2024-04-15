2d7f80487899f94dcff9aeb652f91f53f444d019
NaveenJoyGit <naveenjoy.mec@gmail.com>
2024-04-15 16:56:21 +0530

    Initial Commit

---
+++ src/SnakeGame/Gameframe.java
@@ -0,0 +1,16 @@
+package SnakeGame;
+
+import javax.swing.*;
+
+public class Gameframe extends JFrame {
+    Gameframe(){
+        this.add(new GamePanel());
+        this.setTitle("Snake Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
